a6cf495b08e3861608b167ef250a28758ba8b5cb
Michael Rodriguez <miarodriguezur@unal.edu.co>
2024-07-13 23:15:04 -0500

    Se setea la imagen del producto a la celda informacion producto

--- app/src/main/java/aplicacionesmoviles/avanzado/todosalau/reto1_panaderia/view/ProductManagerCellAdapter.java
+++ app/src/main/java/aplicacionesmoviles/avanzado/todosalau/reto1_panaderia/view/ProductManagerCellAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,6 +18,7 @@ import java.util.List;
 import aplicacionesmoviles.avanzado.todosalau.reto1_panaderia.ListProductsActivity;
 import aplicacionesmoviles.avanzado.todosalau.reto1_panaderia.model.Producto;
 import aplicacionesmoviles.avanzado.todosalau.reto1_panaderia.R;
+import aplicacionesmoviles.avanzado.todosalau.reto1_panaderia.presenter.ProductsName;
 
 public class ProductManagerCellAdapter extends ArrayAdapter<Producto> {
     private final List<Producto> internalProducts;
@@ -62,21 +64,24 @@ public class ProductManagerCellAdapter extends ArrayAdapter<Producto> {
 
         // Si el producto es válido, establecer los valores en la vista
         if (product != null) {
-            // Obtener las vistas de nombre y precio
+            // Obtener las vistas de la celda
             TextView textViewCategory = view.findViewById(R.id.textInfoCategory);
             TextView textViewName = view.findViewById(R.id.textInfoProducName);
             TextView textViewPrice = view.findViewById(R.id.textInfoCost);
             TextView textViewAmount = view.findViewById(R.id.textInfoAmount);
+            ImageView imageProduct = view.findViewById(R.id.imageViewProduct);
 
             // Obtener los botones de edición y eliminación
             Button buttonEdit = view.findViewById(R.id.buttonEdit);
             Button buttonDelete = view.findViewById(R.id.buttonDelete);
 
-            // Establecer el nombre y el precio del producto en los TextView
+            // Establecer los valores en los campos de la celda.
             textViewCategory.setText(product.getCategoria());
             textViewName.setText(product.getNombreProducto());
+            String productName = formatProductName(String.valueOf(product.getNombreProducto()));
             textViewPrice.setText(String.valueOf(product.getPrecioUnidad()));
             textViewAmount.setText(String.valueOf(product.getCantidadStock()));
+            showImage(imageProduct, ProductsName.valueOf(productName));
 
             // Asignar listeners a los botones
             buttonEdit.setOnClickListener(v -> {
@@ -105,5 +110,71 @@ public class ProductManagerCellAdapter extends ArrayAdapter<Producto> {
         internalProducts.clear();
         internalProducts.addAll(collection);
         notifyDataSetChanged();
+    }
+
+    private String formatProductName(String productName) {
+        String productStringFormated = productName.toUpperCase();
+        return productStringFormated.replace(" ", "_");
+    }
+
+    private void showImage(ImageView imageProduct, ProductsName product) {
+        switch (product) {
+            case PAN_BRETON:
+                imageProduct.setImageResource(R.drawable.pan_breton);
+                break;
+            case PAN_CON_CHOCOLATE:
+                imageProduct.setImageResource(R.drawable.pan_con_chocolate);
+                break;
+            case PAN_DE_MOLDE:
+                imageProduct.setImageResource(R.drawable.pan_de_molde);
+                break;
+            case PAN_EN_TRENZA:
+                imageProduct.setImageResource(R.drawable.pan_en_trenza);
+                break;
+            case PAN_HAMBURGUEZA:
+                imageProduct.setImageResource(R.drawable.pan_hamburguesa);
+                break;
+            case PAN_MANCHEGO:
+                imageProduct.setImageResource(R.drawable.pan_manchego);
+                break;
+            case POSTRE_HELADO_TENTACION:
+                imageProduct.setImageResource(R.drawable.postre_helado_tentacion);
+                break;
+            case POSTRE_MAGDALENA:
+                imageProduct.setImageResource(R.drawable.postre_magdalena);
+                break;
+            case POSTRE_MUFFINS:
+                imageProduct.setImageResource(R.drawable.postre_muffins);
+                break;
+            case POSTRE_ROLLS_CANELA:
+                imageProduct.setImageResource(R.drawable.postre_rolls_canela);
+                break;
+            case POSTRE_TARTA_INTEGRAL:
+                imageProduct.setImageResource(R.drawable.postre_tarta_integral);
+                break;
+            case POSTRE_TARTA_ZANAHORIA:
+                imageProduct.setImageResource(R.drawable.postre_tarta_zanahoria);
+                break;
+            case DONA_BACKED:
+                imageProduct.setImageResource(R.drawable.dona_baked);
+                break;
+            case DONA_CHOCOLATE_BLANCO:
+                imageProduct.setImageResource(R.drawable.dona_chocolate_blanco);
+                break;
+            case DONA_CHOCOLATE_FROSTED:
+                imageProduct.setImageResource(R.drawable.dona_chocolate_frosted);
+                break;
+            case DONA_COMBO_BOSTON:
+                imageProduct.setImageResource(R.drawable.dona_combo_boston);
+                break;
+            case DONA_SPUDNUT:
+                imageProduct.setImageResource(R.drawable.dona_spudnut);
+                break;
+            case DONA_STRAWBERRY_FROSTED:
+                imageProduct.setImageResource(R.drawable.dona_strawberry_frosted);
+                break;
+            default:
+                imageProduct.setImageResource(R.drawable.ic_arrow_back);
+        }
     }
 }
